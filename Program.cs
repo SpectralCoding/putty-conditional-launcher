00d744a7f77c5e83a3e7ad4882d55d6c6874ec85
Caesar Kabalan <caesar.kabalan@gmail.com>
2015-02-25 16:50:06 -0700

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PuTTYConditionalLauncher {
+	class Program {
+		static void Main(string[] args) {
+		}
+	}
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("PuTTY Conditional Launcher")]
+[assembly: AssemblyDescription("Launch different PuTTY session based on command-line input.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SpectralCoding")]
+[assembly: AssemblyProduct("PuTTY Conditional Launcher")]
+[assembly: AssemblyCopyright("Copyright© 2015")]
+[assembly: AssemblyTrademark("SpectralCoding")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c6e455ba-c71e-4fc1-8046-ed54da6c9661")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.0.1.16491")]
+[assembly: AssemblyFileVersion("0.0.1.16491")]
